f516667b3cc733227a31c44a49d8da08c683dd69
Varun Nath <nathvarun@hotmail.com>
2019-12-06 10:35:15 +0530

    #2 Project Files

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, Easing } from "react-native";
 import { NavigationNativeContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createStackNavigator, TransitionPresets, CardStyleInterpolators } from "@react-navigation/stack";
 import { useIsFocused } from "@react-navigation/core";
 
 const Stack = createStackNavigator();
@@ -21,10 +21,7 @@ const HomeScreen = ({ navigation }) => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>HomeScreen</Text>
-      <Button
-        title="Go To Settings Screen"
-        onPress={() => navigation.navigate("Settings")}
-      />
+      <Button title="Go To Settings Screen" onPress={() => navigation.navigate("Settings")} />
     </View>
   );
 };
@@ -32,22 +29,48 @@ const SettingsScreen = ({ navigation }) => {
   const isFocused = useIsFocused();
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text style={{ color: isFocused ? "green" : "black" }}>
-        SettingsScreen
-      </Text>
+      <Text style={{ color: isFocused ? "green" : "black" }}>SettingsScreen</Text>
       <Button title="Go To Home Screen" onPress={() => navigation.goBack()} />
     </View>
   );
 };
+
+const config = {
+  animation: "spring",
+  config: {
+    stiffness: 1000,
+    damping: 50,
+    mass: 3,
+    overshootClamping: false,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01
+  }
+};
+
+const closeConfig = {
+  animation: "timing",
+  config: {
+    duration: 500,
+    easing: Easing.linear
+  }
+};
 export default function App() {
   return (
     <NavigationNativeContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          options={{ title: "My Home Screen" }}
-          name="Home"
-          component={HomeScreen}
-        />
+      <Stack.Navigator
+        screenOptions={{
+          gestureEnabled: true,
+          gestureDirection: "horizontal",
+          cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS
+          // transitionSpec: {
+          //   open: config,
+          //   close: closeConfig
+          // }
+        }}
+        headerMode="float"
+        animation="fade"
+      >
+        <Stack.Screen options={{ title: "My Home Screen" }} name="Home" component={HomeScreen} />
         <Stack.Screen name="Settings" component={SettingsScreen} />
       </Stack.Navigator>
     </NavigationNativeContainer>
